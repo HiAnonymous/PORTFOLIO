3703406f5d12543d281be0a28a1d06f2ecc290c8
HiAnonymous <149861302+HiAnonymous@users.noreply.github.com>
2024-02-27 15:47:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
-let skills = document.querySelector('.skills');
+let skills = document.querySelector('skills');
 let navLinks = document.querySelector('header nav a');
 
 window.onscroll = () => {
-    sections.forEach(sec => {
+    skills.forEach(sec => {
         let top=window.scrollY;
         let offset = sec.offsetTop -150;
         let height = sec.offsetHeight;
