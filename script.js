7c1a0372067093e176c47fbe4c5fbee7eb684809
Abhishek Mudit <149861302+HiAnonymous@users.noreply.github.com>
2025-11-05 15:35:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ window.onscroll = () => {
         if(top >= offset && top< offset+height){
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a [href*=' + id + ' ]').classList.add('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             })
         }
     })
@@ -22,3 +22,4 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
+
